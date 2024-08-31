6cde915a5213cdc33b8e8b9e0a31aef0a8a5195b
telemetriajuan <juan.rodriguezr@comunidad.iush.edu.co>
2024-08-31 10:15:40 -0500

    Update subirdatos.php

--- subirdatos.php
+++ subirdatos.php
@@ -5,11 +5,12 @@ require_once 'accesoBD.php';
 $objectResponse = new stdClass();
 date_default_timezone_set("UTC");
 
-if (isset($_POST['valor']) && isset($_POST['tipo'])) {
+if (isset($_POST['valor']) && isset($_POST['tipo'] && isset($_POST['id_dispositivo'])) {
     $valor = mysqli_real_escape_string(conexion::conectar(), $_POST['valor']);
     $tipo = mysqli_real_escape_string(conexion::conectar(), $_POST['tipo']);
+    $id_dispositivo = mysqli_real_escape_string(conexion::conectar(), $_POST['id_dispositivo']);
   
-    $objectResponse = accesoBD::subirDatos($valor, $tipo);
+    $objectResponse = accesoBD::subirDatos($valor, $tipo, $id_dispositivo);
     echo (json_encode($objectResponse));
   }else{
     $objectResponse->code = '500';
