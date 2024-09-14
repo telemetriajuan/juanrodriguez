b5405ee858f4749d711e57a08cb36e7d3cd52cc7
telemetriajuan <juan.rodriguezr@comunidad.iush.edu.co>
2024-09-14 12:11:47 -0500

    Update subirdatos.php

--- subirdatos.php
+++ subirdatos.php
@@ -1,20 +1,20 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-require_once 'accesoBD.php';
+  header('Access-Control-Allow-Origin: *');
+  require_once 'accesoBD.php';
 
-$objectResponse = new stdClass();
-date_default_timezone_set("UTC");
+  $objectResponse = new stdClass();
+  date_default_timezone_set("UTC");
 
-if (isset($_POST['valor']) && isset($_POST['tipo'] && isset($_POST['id_dispositivo'])) {
-    $valor = mysqli_real_escape_string(conexion::conectar(), $_POST['valor']);
-    $tipo = mysqli_real_escape_string(conexion::conectar(), $_POST['tipo']);
-    $id_dispositivo = mysqli_real_escape_string(conexion::conectar(), $_POST['id_dispositivo']);
-  
-    $objectResponse = accesoBD::subirDatos($valor, $tipo, $id_dispositivo);
-    echo (json_encode($objectResponse));
+  if(isset($_POST['valor']) && isset($_POST['tipo']) && isset($_POST['id_dispositivo']) ){
+    $valor= mysqli_real_escape_string(conexion::conectar(),$_POST['valor']);
+    $tipo= mysqli_real_escape_string(conexion::conectar(),$_POST['tipo']);
+    $id_dispositivo= mysqli_real_escape_string(conexion::conectar(),$_POST['id_dispositivo']);
+
+    $objectResponse = accesoBD::subirDatos($valor,$tipo,$id_dispositivo);
+    echo(json_encode($objectResponse));
   }else{
-    $objectResponse->code = '500';
+    $objectResponse->code='500';
     echo (json_encode($objectResponse));
   }
-
+    
 ?>
